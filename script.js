8c622c93efd9e7d78d66f3bf3d44aad345630c68
AtillaTegin <100026159+AtillaTegin@users.noreply.github.com>
2023-11-30 18:47:34 +0400

    Changed code of fetch and make search button working

--- script.js
+++ script.js
@@ -9,30 +9,74 @@ function closeNav() {
 }
 
 const productsContainer = document.querySelector('.products-container');
+let allProducts = [];
 
 const fetchData = function () {
     const request = new XMLHttpRequest();
-    request.open('GET', `https://dummyjson.com/products`);
+    request.open('GET', 'https://dummyjson.com/products');
     request.send();
 
     request.addEventListener('load', function() {
         const data = JSON.parse(this.responseText);
         console.log(data);
-        let allProducts = data.products;
-        for (let i = 0; i < allProducts.length; i++){
-            let main = `<article class="products">
-            <img class="products-image" src="${allProducts[i].thumbnail}" alt="">
+        allProducts = data.products;
+        displayProducts(allProducts);
+    });
+};
+
+const displayProducts = function(products) {
+    productsContainer.innerHTML = '';
+
+    for (let i = 0; i < products.length; i++) {
+        let main = `<article class="products">
+            <img class="products-image" src="${products[i].thumbnail}" alt="">
             <div class="products-details">
-                <h3 class="products-name">${allProducts[i].title}</h3>
-                <h4 class="products-description">${allProducts[i].description}</h4>
-                <p class="products-brand">Brand: <span>${allProducts[i].brand}</span></p>
-                <p class="products-price">Price: <span>${allProducts[i].price}</span>$</p>
-                <p class="products-rating">Rating: <span>${allProducts[i].rating}</span></p>
+                <h3 class="products-name">${products[i].title}</h3>
+                <h4 class="products-description">${products[i].description}</h4>
+                <p class="products-brand">Brand: <span>${products[i].brand}</span></p>
+                <p class="products-price">Price: <span>${products[i].price}</span>$</p>
+                <p class="products-rating">Rating: <span>${products[i].rating}</span></p>
             </div>
-            </article>`;
-            console.log(allProducts[i]);
-            productsContainer.insertAdjacentHTML('beforeend', main);
-        }
-    })
-}
-fetchData();
+        </article>`;
+        productsContainer.insertAdjacentHTML('beforeend', main);
+    }
+};
+
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function() {
+    const inputValue = document.querySelector('.input-bar').value.toLowerCase();
+    const filteredProducts = allProducts.filter(product => product.title.toLowerCase().includes(inputValue));
+
+    displayProducts(filteredProducts);
+});
+
+document.addEventListener('DOMContentLoaded', fetchData);
+
+// const fetchData = function () {
+//     const request = new XMLHttpRequest();
+//     request.open('GET', 'https://dummyjson.com/products');
+//     request.send();
+
+//     request.addEventListener('load', function() {
+//         const data = JSON.parse(this.responseText);
+//         console.log(data);
+//         let allProducts = data.products;
+//         for (let i = 0; i < allProducts.length; i++){
+//             let main = `<article class="products">
+//                 <img class="products-image" src="${allProducts[i].thumbnail}" alt="">
+//                 <div class="products-details">
+//                     <h3 class="products-name">${allProducts[i].title}</h3>
+//                     <h4 class="products-description">${allProducts[i].description}</h4>
+//                     <p class="products-brand">Brand: <span>${allProducts[i].brand}</span></p>
+//                     <p class="products-price">Price: <span>${allProducts[i].price}</span>$</p>
+//                     <p class="products-rating">Rating: <span>${allProducts[i].rating}</span></p>
+//                 </div>
+//             </article>`;
+//             productsContainer.insertAdjacentHTML('beforeend', main);
+//         }
+//     })
+// }
+// fetchData();
+
+
+
