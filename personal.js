933d889f6ab9f9b144e27738b70bd67db37b9f70
AtillaTegin <100026159+AtillaTegin@users.noreply.github.com>
2023-12-02 03:14:56 +0400

    Just trying to do nav bar

--- personal.js
+++ personal.js
@@ -8,34 +8,54 @@ function closeNav() {
     document.getElementById("sideNavbar").style.width = "0";
 }
 
-const productsContainerTwo = document.querySelector('.products-container');
+const productsContainerOne = document.querySelector('.products-container-one');
+const productsContainerTwo = document.querySelector('.products-container-two');
+const productsContainerThree = document.querySelector('.products-container-three');
+let allProducts = [];
 
-const fetchPersonalData = function() {
+const fetchData = function () {
     const request = new XMLHttpRequest();
-    request.open('GET', `https://dummyjson.com/products`);
+    request.open('GET', 'https://dummyjson.com/products');
     request.send();
 
     request.addEventListener('load', function() {
         const data = JSON.parse(this.responseText);
         console.log(data);
-        let allProducts = data.products;
-        for (let i = 0; i < allProducts.length; i++){
-            if(allProducts[i].category === "fragrances" || allProducts[i].category === "skincare"){
-                let main = `<article class="products">
-                    <img class="products-image" src="${allProducts[i].thumbnail}" alt="">
-                    <div class="products-details">
-                    <h3 class="products-name">${allProducts[i].title}</h3>
-                    <h4 class="products-description">${allProducts[i].description}</h4>
-                    <p class="products-brand">Brand: <span>${allProducts[i].brand}</span></p>
-                    <p class="products-price">Price: <span>${allProducts[i].price}</span>$</p>
-                    <p class="products-rating">Rating: <span>${allProducts[i].rating}</span></p>
-                    </div>
-                    </article>`;
-                console.log(main);
-                productsContainerTwo.insertAdjacentHTML('beforeend', main);
-            }
-        }
-    })
+        allProducts = data.products;
+        displayProducts(allProducts, productsContainerOne);
+    });
+};
+
+const displayProducts = function(products, container) {
+    container.innerHTML = '';
+
+    for (let i = 0; i < products.length; i++) {
+        let main = `<article class="products">
+            <img class="products-image" src="${products[i].thumbnail}" alt="">
+            <div class="products-details">
+                <h3 class="products-name">${products[i].title}</h3>
+                <h4 class="products-description">${products[i].description}</h4>
+                <p class="products-brand">Brand: <span>${products[i].brand}</span></p>
+                <p class="products-price">Price: <span>${products[i].price}</span>$</p>
+                <p class="products-rating">Rating: <span>${products[i].rating}</span></p>
+            </div>
+        </article>`;
+        container.insertAdjacentHTML('beforeend', main);
+    }
+};
+
+for(let j = 0; j < allProducts.length; j++){
+    console.log(allProducts[j].category);
 }
 
-fetchPersonalData();
+
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function() {
+    const inputValue = document.querySelector('.input-bar').value.toLowerCase();
+    const filteredProducts = allProducts.filter(product => product.title.toLowerCase().includes(inputValue));
+
+    displayProducts(filteredProducts, productsContainerOne);
+});
+
+
+document.addEventListener('DOMContentLoaded', fetchData);
--- script.js
+++ script.js
@@ -42,6 +42,8 @@ const displayProducts = function(products) {
     }
 };
 
+const filterProducts = function() {}
+
 const searchButton = document.querySelector('.search-button');
 searchButton.addEventListener('click', function() {
     const inputValue = document.querySelector('.input-bar').value.toLowerCase();
@@ -52,31 +54,7 @@ searchButton.addEventListener('click', function() {
 
 document.addEventListener('DOMContentLoaded', fetchData);
 
-// const fetchData = function () {
-//     const request = new XMLHttpRequest();
-//     request.open('GET', 'https://dummyjson.com/products');
-//     request.send();
 
-//     request.addEventListener('load', function() {
-//         const data = JSON.parse(this.responseText);
-//         console.log(data);
-//         let allProducts = data.products;
-//         for (let i = 0; i < allProducts.length; i++){
-//             let main = `<article class="products">
-//                 <img class="products-image" src="${allProducts[i].thumbnail}" alt="">
-//                 <div class="products-details">
-//                     <h3 class="products-name">${allProducts[i].title}</h3>
-//                     <h4 class="products-description">${allProducts[i].description}</h4>
-//                     <p class="products-brand">Brand: <span>${allProducts[i].brand}</span></p>
-//                     <p class="products-price">Price: <span>${allProducts[i].price}</span>$</p>
-//                     <p class="products-rating">Rating: <span>${allProducts[i].rating}</span></p>
-//                 </div>
-//             </article>`;
-//             productsContainer.insertAdjacentHTML('beforeend', main);
-//         }
-//     })
-// }
-// fetchData();
 
 
 
